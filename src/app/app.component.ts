fe84c4c2cd0b8a4afeaed717543278708f0700e5
omoloye13 <omoloyefauziyat@gmail.com>
2021-03-02 04:39:50 -0800

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import {
+  AddTodo,
+  DeleteTodo,
+  GetTodos,
+  UpdateTodo,
+} from './store/todo.action';
+import { TodoState } from './store/todo.reducer';
+import { getTodos } from './store/todo.selector';
+
+export interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'crud';
+export class AppComponent implements OnInit {
+  public todoForm: FormGroup;
+
+  public todos: Observable<Todo[]>;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private todoStore: Store<TodoState>
+  ) {}
+
+  ngOnInit(): void {
+    this.todoStore.dispatch(new GetTodos());
+    this.todoForm = this.formBuilder.group({
+      title: ['', [Validators.required]],
+    });
+
+    this.todos = this.todoStore.select(getTodos);
+  }
+
+  add() {
+    if (this.todoForm.valid) {
+      const title: string = this.todoForm.get('title').value as string;
+
+      this.todoStore.dispatch(new AddTodo({ todoTitle: title }));
+
+      this.todoForm.reset();
+      this.todoStore.dispatch(new GetTodos());
+    }
+  }
+
+  update(todo: Todo) {
+    this.todoStore.dispatch(new UpdateTodo({ todoId: todo.id }));
+    this.todoStore.dispatch(new GetTodos());
+  }
+
+  delete(todo: Todo) {
+    this.todoStore.dispatch(new DeleteTodo({ todoId: todo.id }));
+    this.todoStore.dispatch(new GetTodos());
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { todoReducer } from './store/todo.reducer';
+import { TodoEffects } from './store/todo.effect';
+// import { todoReducer } from './state/todo.reducers';
+import { TodoService } from './todo.service';
 
 @NgModule({
   declarations: [
@@ -10,9 +20,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgbModule,
+    StoreModule.forRoot({todos:todoReducer}, {}),
+    EffectsModule.forRoot([TodoEffects]),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
+
   ],
-  providers: [],
+  providers: [TodoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
